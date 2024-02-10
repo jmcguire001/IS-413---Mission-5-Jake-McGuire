9fdc1b40950d1f30ee36043b45e3bb9773371084
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-02-09 23:00:50 -0700

    Deleted some more stuff i don't need

--- IS 413 - Mission 5/Controllers/HomeController.cs
+++ IS 413 - Mission 5/Controllers/HomeController.cs
@@ -6,14 +6,6 @@ namespace IS_413___Mission_5.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        // Constructor. The logger is to keep track of the errors that occur in the app
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         // Index method. This is the default method for the app home page
         public IActionResult Index()
         {
