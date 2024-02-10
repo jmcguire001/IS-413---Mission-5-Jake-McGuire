4db74947656f33b5bbcba52b9f4d412b10267d8d
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-02-09 22:43:11 -0700

    Commented out code, finalized the program

--- IS 413 - Mission 5/Controllers/HomeController.cs
+++ IS 413 - Mission 5/Controllers/HomeController.cs
@@ -8,24 +8,29 @@ namespace IS_413___Mission_5.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        // Constructor. The logger is to keep track of the errors that occur in the app
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        // Index method. This is the default method for the app home page
         public IActionResult Index()
         {
             return View();
         }
 
+        // Quote method. This method is for the quote page
         public IActionResult Quote()
         {
             return View();
         }
 
+        // Error method. The response cache is to prevent the page from being cached
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            // Keep track of the error and display it in the error message on the page
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
